2617509fdbb9d36c1739874dd8608d33efd8f241
Vlad Crisan <vlad.crishan20@gmail.com>
2018-04-12 21:32:22 +0300

    Implementing Firebase

--- app/src/main/java/com/tabita/mydiagnosistreatment/SignUpActivity.java
+++ app/src/main/java/com/tabita/mydiagnosistreatment/SignUpActivity.java
@@ -1,21 +1,101 @@
 package com.tabita.mydiagnosistreatment;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText mEmailView;
+    private EditText mPasswordView;
+    private EditText mPasswordConfirmView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        mEmailView = findViewById(R.id.email);
+        mPasswordView = findViewById(R.id.password);
+        mPasswordConfirmView = findViewById(R.id.confirm_password);
+
+        mPasswordConfirmView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void signUp(View view) {
-        startActivity(new Intent(this, ClientActivity.class));
+        attemptLogin();
     }
+
+    private void attemptLogin() {
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+        String passwordConfirm = mPasswordConfirmView.getText().toString();
+
+        if (!validateInput(email, password, passwordConfirm)) {
+            return;
+        }
+
+        LoginActivity.mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            LoginActivity.user = LoginActivity.mAuth.getCurrentUser();
+                            startActivity(new Intent(SignUpActivity.this, ClientActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private Boolean validateInput(String email, String password, String passwordConfirm) {
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            mEmailView.requestFocus();
+            return false;
+        } else if (!email.contains("@")) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            mEmailView.requestFocus();
+            return false;
+        }
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(password) || password.length() < 6) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            mPasswordView.requestFocus();
+            return false;
+        } else if (!password.equals(passwordConfirm)) {
+            mPasswordConfirmView.setError(getString(R.string.error_not_matching_passwords));
+            mPasswordConfirmView.requestFocus();
+            return false;
+        }
+
+        return true;
+    }
+
 }
