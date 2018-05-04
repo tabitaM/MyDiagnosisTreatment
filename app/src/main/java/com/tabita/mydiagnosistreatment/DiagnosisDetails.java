f7b672245c81cb74d80f5a9996073d0d43a1e11f
Vlad Crisan <vlad.crishan20@gmail.com>
2018-05-04 22:05:59 +0300

    Adding ListView in diagnosis details layout

--- app/src/main/java/com/tabita/mydiagnosistreatment/DiagnosisDetails.java
+++ app/src/main/java/com/tabita/mydiagnosistreatment/DiagnosisDetails.java
@@ -2,24 +2,20 @@ package com.tabita.mydiagnosistreatment;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.tabita.mydiagnosistreatment.model.Diagnosis;
+import com.tabita.mydiagnosistreatment.model.Medication;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class DiagnosisDetails extends AppCompatActivity {
 
     private TextView diagnosisNameView;
     private TextView diagnosisPeriodView;
-    private ListView medicationNameDoseView;
-    //private TextView medicationDoseView;
+    private ListView medicationListView;
     private TextView notesView;
-    private ArrayAdapter<String> adapter;
-    private ArrayList<String> nameDoseList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +24,20 @@ public class DiagnosisDetails extends AppCompatActivity {
 
         Diagnosis diagnosis = (Diagnosis) getIntent().getSerializableExtra(ClientActivity.KEY);
 
+        // Diagnosis Name
         diagnosisNameView = findViewById(R.id.name);
         diagnosisNameView.setText(diagnosis.getName());
 
+        // Diagnosis Period
         diagnosisPeriodView = findViewById(R.id.period);
         diagnosisPeriodView.setText(diagnosis.getTreatment().getPeriod());
 
-        medicationNameDoseView = findViewById(R.id.medication_name_dose);
-        adapter = new ArrayAdapter<>(DiagnosisDetails.this, R.layout.activity_diagnosis_details);
-        medicationNameDoseView.setAdapter(adapter);
+        // Medication List
+        List<Medication> medicationList = diagnosis.getTreatment().getMedication();
+        medicationListView = findViewById(R.id.medication_list);
+        medicationListView.setAdapter(new MedicationAdapter(this, medicationList));
 
-        /*medicationDoseView = findViewById(R.id.medication_dose);
-        medicationDoseView.setText(diagnosis.getTreatment().getMedication().get(0).getDose());*/
-
+        // Notes
         notesView = findViewById(R.id.notes);
         notesView.setText((diagnosis.getTreatment().getNotes()));
 
---
+++ app/src/main/java/com/tabita/mydiagnosistreatment/MedicationAdapter.java
@@ -0,0 +1,42 @@
+package com.tabita.mydiagnosistreatment;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.tabita.mydiagnosistreatment.model.Medication;
+
+import java.util.List;
+
+public class MedicationAdapter extends ArrayAdapter<Medication> {
+
+    public MedicationAdapter(Context context, List<Medication> medications) {
+        super(context, 0, medications);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // Get the data item for this position
+        Medication medication = getItem(position);
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.medication_list_item, parent, false);
+        }
+
+        // Lookup view for data population
+        TextView nameView = convertView.findViewById(R.id.name);
+        TextView doseView = convertView.findViewById(R.id.dose);
+
+        // Populate the data into the template view using the data object
+        nameView.setText(medication.getName());
+        doseView.setText(medication.getDose());
+
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
