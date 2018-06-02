8d79e5829e6cd3c42e017cc3cf18b0151c40acb5
Vlad Crisan <vlad.crishan20@gmail.com>
2018-06-02 22:39:43 +0300

    nada

--- app/src/main/java/com/tabita/mydiagnosistreatment/ClientActivity.java
+++ app/src/main/java/com/tabita/mydiagnosistreatment/ClientActivity.java
@@ -10,6 +10,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
